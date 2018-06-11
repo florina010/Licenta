fe93c2a71f40f88b6e0ab9caa3772c508dd56b3e
Florina <dascal.florina@gmail.com>
2018-06-11 19:11:08 +0300

    update service worker's scope

--- public/js/display.js
+++ public/js/display.js
@@ -1,5 +1,26 @@
 "use strict";
+  navigator.serviceWorker.register('service-worker.js', {scope: './'}).then(reg => {
+  reg.installing; // the installing worker, or undefined
+  reg.waiting; // the waiting worker, or undefined
+  reg.active; // the active worker, or undefined
 
+  reg.addEventListener('updatefound', () => {
+    // A wild service worker has appeared in reg.installing!
+    const newWorker = reg.installing;
+
+    newWorker.state;
+    // "installing" - the install event has fired, but not yet complete
+    // "installed"  - install complete
+    // "activating" - the activate event has fired, but not yet complete
+    // "activated"  - fully active
+    // "redundant"  - discarded. Either failed install, or it's been
+    //                replaced by a newer version
+
+    newWorker.addEventListener('statechange', () => {
+      // newWorker.state has changed
+    });
+  });
+});
 if ('serviceWorker' in navigator) {
   var lastStatus = true;
   setTimeout(function() {
@@ -29,28 +50,28 @@ if ('serviceWorker' in navigator) {
 }
 
 $(document).ready(function() {
-  navigator.serviceWorker.register('service-worker.js').then(reg => {
-  reg.installing; // the installing worker, or undefined
-  reg.waiting; // the waiting worker, or undefined
-  reg.active; // the active worker, or undefined
-
-  reg.addEventListener('updatefound', () => {
-    // A wild service worker has appeared in reg.installing!
-    const newWorker = reg.installing;
-
-    newWorker.state;
-    // "installing" - the install event has fired, but not yet complete
-    // "installed"  - install complete
-    // "activating" - the activate event has fired, but not yet complete
-    // "activated"  - fully active
-    // "redundant"  - discarded. Either failed install, or it's been
-    //                replaced by a newer version
-
-    newWorker.addEventListener('statechange', () => {
-      // newWorker.state has changed
-    });
-  });
-});
+//   navigator.serviceWorker.register('service-worker.js').then(reg => {
+//   reg.installing; // the installing worker, or undefined
+//   reg.waiting; // the waiting worker, or undefined
+//   reg.active; // the active worker, or undefined
+//
+//   reg.addEventListener('updatefound', () => {
+//     // A wild service worker has appeared in reg.installing!
+//     const newWorker = reg.installing;
+//
+//     newWorker.state;
+//     // "installing" - the install event has fired, but not yet complete
+//     // "installed"  - install complete
+//     // "activating" - the activate event has fired, but not yet complete
+//     // "activated"  - fully active
+//     // "redundant"  - discarded. Either failed install, or it's been
+//     //                replaced by a newer version
+//
+//     newWorker.addEventListener('statechange', () => {
+//       // newWorker.state has changed
+//     });
+//   });
+// });
   var navItems = $('.admin-menu li > a');
   var navListItems = $('.admin-menu li');
   var allWells = $('.admin-content');
